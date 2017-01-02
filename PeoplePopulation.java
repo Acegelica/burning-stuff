5f2afcfc8ced7f9871cbf2f68f35317928e67bea
Acegelica <skyler.matuska@gmail.com>
2017-01-02 13:00:09 -0500

    made grow work
    many thing
    also made the getters and setters actually work, everest

--- PeoplePopulation.java
+++ PeoplePopulation.java
@@ -6,6 +6,10 @@ public class PeoplePopulation{
   //The time unit is in years, for the sake of understanding
   private double popWCookstove;
   private double popWOut;
+  public double withCC;
+  public double final OUT_CC=7.720802564*Math.pow(10,7);
+  public double final GROWTH_RATE=2.41;
+  public double final MIDPOINT=50;
 
   public PeoplePopulation(double startingWCookstove, double startingWOut){
     popWCookstove = startingWCookstove;
@@ -20,34 +24,26 @@ public class PeoplePopulation{
     return popWCookstove;
   }
   public double getPopWOut(){
-    return popWCookstove;
+    return popWOut;
   }
   //The setters should see no use as they are not how the class is supposed to
   //interact with these values
-  public void setPopWCookstove(){
-    return popWCookstove;
+  public void setPopWCookstove(double newWC){
+    popWCookstove = newWC;
   }
-  public void setPopWOut(){
-    return popWCookstove;
+  public void setPopWOut(double newWO){
+    popWOut = newWO;
   }
   /**
   * This method has each of the populations grow from what would be considered
-  * natural factors, such as avalibilty of food, avoiding desieses, and having
+  * natural factors, such as avalibilty of food, avoiding disease, and having
   * clean vs. unclean air
   * @author Everest Bloomer
   */
-  public void annualGrowth(){
-    popWCookstove //Need to decide exactly how these would increase, and whether or not we want to hard code it in
-                  //May have variables of the growthrate that can be inputed at the beginning of the creation of the object
-    popWOut       //Similar to the other section, we need to decide how to have the growth be done,
-                  //Talking about like exponential or logisitic growth or some other model of growth
-    //Just making a note that the population doesn't actually care about the rest of the global population since we
-    //Are only tracking the delta T with the adjustments, not the actual current T.
-    //Unfortinantly, the current formula I have to calculate delta T requires knowledge of the current T.
-    // dT = [5.35*ln(PPMC/PPMS)]/[4*CT^4 * stefan] * CT;
-    // change in temperature equals the change in flux divided by 4 times the flux times current temprature
-      // where the change in flux is given by 5.35 times the natural log of the quotient between the current paarts per million of CO2 and the starting parts per million
-      // and flux is given by the current temperature to the 4th power and multiplying by the Stefan constant
+  public void grow(int year){
+    popWCookstove=withCC/(1+Math.e^((-GROWTH_RATE)*(year-MIDPOINT)))      
+    popWOut=OUT_CC/(1+Math.e^((-GROWTH_RATE)*(year-MIDPOINT)))
+
   }
 
 
